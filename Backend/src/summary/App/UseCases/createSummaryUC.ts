0759735f712fea4e551af6d07018d22316651def
neytor <ortegajoseweb02@gmail.com>
2023-11-06 22:16:23 -0300

    database update

--- Backend/src/summary/App/UseCases/createSummaryUC.ts
+++ Backend/src/summary/App/UseCases/createSummaryUC.ts
@@ -5,10 +5,26 @@ import { SummaryCreator } from '../../Domain/Entities/SummaryCreator'
 class createSummaryUC {
   constructor(private readonly repository: IsummaryRepo<Summary>) {}
 
-  public cresateSummary(name: string) {
+  public cresateSummary(
+    name: string,
+    pdf: string,
+    sum_desc: string,
+    subject: string,
+    career: string,
+    lenght: number,
+    up_date: string,
+    likes: number
+  ) {
     const data = {
       id: this.repository.getNextID(),
-      name: name
+      name: name,
+      likes: likes,
+      pdf: pdf,
+      sum_desc: sum_desc,
+      subject: subject,
+      career: career,
+      lenght: lenght,
+      up_date: up_date
     }
     const newSum = SummaryCreator.createSummary(data)
     this.repository.create(newSum)
--- Backend/src/summary/Infra/Repositories/SummaryRepository.ts
+++ Backend/src/summary/Infra/Repositories/SummaryRepository.ts
@@ -1,40 +1,55 @@
 import { Summary } from 'src/summary/Domain/Entities/Summary'
 import { IsummaryRepo } from './IsummaryRepo'
+import mysql, { ConnectionOptions, RowDataPacket } from 'mysql2/promise'
+import { randomUUID } from 'crypto'
 
+const access: ConnectionOptions = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'summary_db'
+}
+const connection = mysql.createPool(access)
 export default class SummaryRepository implements IsummaryRepo<Summary> {
-  private summaries: Summary[]
-  private idCounter: number
-  constructor() {
-    this.idCounter = 0
-    this.summaries = [
-      { id: this.idCounter++, name: 'Sumary 1' },
-      { id: this.idCounter++, name: 'Sumary 2' }
+  getNextID(): `${string}-${string}-${string}-${string}-${string}` {
+    return randomUUID()
+  }
+  async getAll() {
+    const sql = 'SELECT * FROM summary;'
+    const [rows] = await connection.execute<RowDataPacket[]>(sql)
+    return rows
+  }
+  async create(sumary: Summary) {
+    const sql = 'INSERT INTO Summaries(id,name,lenght,up_date,sum_desc,pdf,career,subject,likes) VALUES(?, ?);'
+    const values = [
+      sumary.id,
+      sumary.name,
+      sumary.lenght,
+      sumary.up_date,
+      sumary.sum_desc,
+      sumary.pdf,
+      sumary.career,
+      sumary.subject,
+      sumary.likes
     ]
+    await connection.execute(sql, values)
   }
-  getNextID(): number {
-    return this.idCounter++
-  }
-  getAll() {
-    return this.summaries
-  }
-  create(sumary: Summary) {
-    this.summaries.push(sumary)
-  }
-  getOne(id: number): Summary | undefined {
-    return this.summaries.find((summary) => summary.id === id)
+  async getOne(id: number) {
+    const sql = 'SELECT * FROM Summaries WHERE id = ?;'
+    const values = [id]
+    const [rows] = await connection.execute<RowDataPacket[]>(sql, values)
+    return rows[0]
   }
   delete(id: number): number {
-    const summaryIndex = this.summaries.findIndex((summary) => summary.id === id)
-    this.summaries.splice(summaryIndex, 1)
-    return summaryIndex
+    const sql = 'DELETE FROM Summaries WHERE id = ?;'
+    const values = [id]
+    connection.execute(sql, values)
+    return id
   }
-  update(id: number, name: string): Summary | number {
-    const sumaryIndex = this.summaries.findIndex((summary) => summary.id === id)
-    if (sumaryIndex !== -1) {
-      this.summaries[sumaryIndex].name = name
-      return this.summaries[sumaryIndex]
-    } else {
-      return sumaryIndex
-    }
+  async update(id: number, name: string) {
+    const sql = 'UPDATE Summaries SET name = ? WHERE id = ?;'
+    const values = [name, id]
+    const [rows] = await connection.execute<RowDataPacket[]>(sql, values)
+    return rows[0]
   }
 }
